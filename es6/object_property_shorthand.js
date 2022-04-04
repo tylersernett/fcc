6f4807bcdcd02c8b69b066bf393e676b43fe77aa
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2022-04-04 16:57:15 -0400

    Add object_property_shorthand.js

---
+++ es6/object_property_shorthand.js
@@ -0,0 +1,6 @@
+const createPerson = (name, age, gender) => ({
+    // Only change code below this line
+    name, age, gender
+    // Only change code above this line
+  });
+  //https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/es6/write-concise-object-literal-declarations-using-object-property-shorthand
