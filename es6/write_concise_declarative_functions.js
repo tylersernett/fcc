e097f6c8df8d8356897323a4218be66f1e9fc752
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2022-04-04 17:03:19 -0400

    Add write_concise_declarative_functions.js

---
+++ es6/write_concise_declarative_functions.js
@@ -0,0 +1,22 @@
+// Only change code below this line
+const bicycle = {
+    gear: 2,
+    setGear(newGear) {
+      this.gear = newGear;
+    }
+  };
+  // Only change code above this line
+  bicycle.setGear(3);
+  console.log(bicycle.gear);
+  //https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/es6/write-concise-declarative-functions-with-es6
+  /*
+    sayHello: function() {
+      return `Hello! My name is ${this.name}.`;
+    }
+  remove :function
+    sayHello()
+  
+    functionName(vars) {
+      
+    }
+  */
