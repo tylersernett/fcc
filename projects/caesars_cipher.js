53aa88aa39a3cd1ff17f49ca456f4bc770f8aee4
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2022-04-07 17:30:58 -0400

    Add caesars_cipher.js

---
+++ projects/caesars_cipher.js
@@ -0,0 +1,15 @@
+function rot13(str, offset=13) {
+    var decoded = "";
+     
+    for (var i = 0; i < str.length; i++) {
+      var raw = str.charCodeAt(i);
+      if (raw >=65 && raw <= 90 ) {
+        raw +=offset;
+        if (raw > 90) {
+          raw -= 26; //return to start of alphabet
+        }
+      }  
+      decoded += String.fromCharCode(raw);
+    }
+    return decoded;
+  }
