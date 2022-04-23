b4295b566e1eb020995675659c80eb54b71431a2
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2022-04-23 03:57:46 -0400

    Cleanup colors & design

--- front-end/projects/random-quote-machine/index.js
+++ front-end/projects/random-quote-machine/index.js
@@ -1,10 +1,11 @@
 function App() {
     const [quotes, setQuotes] = React.useState([]);
     const [randomQuote, setRandomQuote] = React.useState("");
+    const [color, setColor] = React.useState("#fff");
 
     //useEffect args: callback function, array of dependencies
     React.useEffect(() => {
-        async function fetchData() {  //async/wwait vs promises: https://levelup.gitconnected.com/async-await-vs-promises-4fe98d11038f
+        async function fetchData() {  //async/await vs promises: https://levelup.gitconnected.com/async-await-vs-promises-4fe98d11038f
             const response = await fetch("https://type.fit/api/quotes")
             const data = await response.json();
 
@@ -16,39 +17,63 @@ function App() {
     }, [])
 
     const getRandomQuote = (arr) => {
+        const colors = [
+            '#16a085',
+            '#27ae60',
+            '#2c3e50',
+            '#f39c12',
+            '#e74c3c',
+            '#9b59b6',
+            '#FB6964',
+            '#342224',
+            '#472E32',
+            '#BDBB99',
+            '#77B1A9',
+            '#73A857'];
+        
         let randomIndex = Math.floor(Math.random() * arr.length); //don't forget to add () to random()
         setRandomQuote(arr[randomIndex]);
+
+        let randColorIndex = Math.floor(Math.random() * colors.length);
+        setColor(colors[randColorIndex]);
     }
 
 
     return (
-        <div className="container pt-4 ">
-            
-            <div className="jumbotron " id="quote-box"><h4>Quote Machine</h4>
+        <div className = "bag" style={{backgroundColor: color, minHeight:"100vh"}} >
+            <div className="container custom-container pt-5 "  style={{backgroundColor: color}}>
+                <div className="jumbotron custom-jumbo py-4" id="quote-box" ><h3>Random Quote Machine</h3>
+                    <div className="card" >
+                        {/* if a random quote is loaded, display it. otherwise, display "loading..." */}
+                        {randomQuote ?
+                            // empty tags for JSX:
+                            <>
+                                <div id="text" className="card-body" ><h4>"{randomQuote.text}"</h4></div>
+                                <div id="author" className="h-50 card-header text-right">– {randomQuote.author || "Anonymous"}</div>
+                            </>
+                            :
+                            <div className="card-body text-center">
+                                <div class="spinner-border text-primary " role="status">
+                                    <span class="visually-hidden "></span>
+                                </div><h2></h2>
+                            </div>}
+                    </div>
 
-                <div className="card" >
-                    {/* if a random quote is loaded, display it. otherwise, display "loading..." */}
-                    {randomQuote ? (
-                        // empty tags for JSX:
-                        <>
-                            <div id="text" className="card-body">"{randomQuote.text}"</div>
-                            <div id="author" className="h-50 card-header text-right">-{randomQuote.author || "Anonymous"}</div>
-                        </>
-                    ) : <h2>Loading...</h2>}
-                    {/* if no arguments being passed, just do onClick={getNewQuote}
+                    <div className=" pt-1 text-right">
+                        {/* if no arguments being passed, just do onClick={getNewQuote}
                     but to pass arguments, call anon function (), as in onClick={ ()=> functionName(argumentName){} */}
+                        <button id="new-quote" onClick={() => getRandomQuote(quotes)} className="btn btn-dark m-1"  style={{backgroundColor: color}}>Randomize</button>
+                        <a id="tweet-quote" target="_blank" href={
+                            "https://twitter.com/intent/tweet?hashtags=quotes&text=" + encodeURIComponent('"' + randomQuote.text + '"') + encodeURIComponent(' -' + randomQuote.author)}>
+                            <button className="btn btn-dark"  style={{backgroundColor: color, color:"white"}}>Tweet
+                            </button>
+                        </a>
+                    </div>
 
                 </div>
-                <div className=" pt-1 text-right">
-                    <button id="new-quote" onClick={() => getRandomQuote(quotes)} className="btn btn-info">Randomize</button>
-                    <a id="tweet-quote" target = "_blank" href={
-                        "https://twitter.com/intent/tweet?hashtags=quotes&text=" + encodeURIComponent('"' + randomQuote.text + '"') + encodeURIComponent(' -' + randomQuote.author)}>
-                        <button  className="btn btn-primary">Tweet
-                        </button>
-                    </a>
-                </div>
-            </div>
-        </div >
+            </div >
+        </div>
+
     );
 }
 
