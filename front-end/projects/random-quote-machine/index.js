3c6d8399d5c2f25368f26bebe30b4b3565c4fa5f
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2022-04-22 21:42:46 -0400

    Display entire quote database

--- front-end/projects/random-quote-machine/index.js
+++ front-end/projects/random-quote-machine/index.js
@@ -1,6 +1,30 @@
 function App() {
+    const [quotes, setQuotes] = React.useState([]);
+    const [randomQuote, setRandomQuote] = React.useState([]);
+
+    //useEffect args: callback function, array of dependencies
+    React.useEffect(() => {
+        async function fetchData() {  //async/wwait vs promises: https://levelup.gitconnected.com/async-await-vs-promises-4fe98d11038f
+            const response = await fetch("https://type.fit/api/quotes")
+            const data = await response.json();
+
+            setQuotes(data);
+            let randomIndex = Math.floor(Math.random * data.length);
+            setRandomQuote(data[randomIndex]);
+        }
+        fetchData();
+
+    }, [])
+
     return (
-        <div>Hello World</div>
+        <div>Hello Worlds
+            {quotes.map(quote => (
+                <div>
+                    <div>"{quote.text}"</div>
+                    <div>-{quote.author}</div>
+                </div>
+            ))}
+        </div>
     );
 }
 
