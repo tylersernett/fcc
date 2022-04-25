a39bcc44165a48f54556711860f86c56d3571d83
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2022-04-25 11:59:37 -0400

    Cleanup formatting

--- front-end/projects/markdown-previewer/index.js
+++ front-end/projects/markdown-previewer/index.js
@@ -8,21 +8,17 @@ function App() {
                 {/* editor  */}
                 <div className="col"><h2>Raw Editor</h2>
                     <textarea
+                        id="editor"
                         placeholder="type raw markdown here"
                         value={text}
-                        onChange={
-                            (event) => {
-                                setText(event.target.value);
-                            }
-                        }>
-                        {/* onChange....: whenever the text changes, use hook to update text variable */}
-
-
+                        onChange={(event) => setText(event.target.value)}>
+                        {/* onChange....: whenever the text changes, use state update hook to update text variable */}
                     </textarea>
                 </div>
+
                 {/* previewer */}
                 <div className="col"><h2>Preview</h2>
-                    <div dangerouslySetInnerHTML={{ __html: marked.parse(text) }}>
+                    <div id="preview" dangerouslySetInnerHTML={{ __html: marked.parse(text) }}>
                         {/* displaying {marked.parse(text)} in divs will output raw preformatted HTML -- 
                 we must set inner HTML instead to ensure the raw data actually gets formatted */}
                     </div>
