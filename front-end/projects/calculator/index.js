e1fe62ac991824dc4f25f7f98707167717d80907
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2022-05-04 18:34:52 -0400

    Add clear entry, round button edges, tweak display margin

--- front-end/projects/calculator/index.js
+++ front-end/projects/calculator/index.js
@@ -4,7 +4,7 @@ function App() {
     [4, "four"], [5, "five"], [6, "six"],  ["x", "multiply"],
     [1, "one"], [2, "two"], [3, "three"],  ["-", "subtract"],
     ["±", "negative"], [0, "zero"], [".", "decimal"], ["+", "add"],
-    ["c", "clear"], ["=", "equals"]];
+    ["C", "clear"], ["CE", "clear-entry"],  ["=", "equals"]];
 
     const [calc, setCalc] = React.useState({
         num: 0,
@@ -94,18 +94,26 @@ function App() {
         });
     }
 
+    //only clear most recent entry
+    const clearEntryClickHandler = () => {
+        setCalc({...calc,
+            num: 0,
+        });
+    }
+
     return (
         <div className="container">
             <div className="calc-body mt-3">
-                <div id="display" className="text-end fs-3 m-2 px-1">{calc.num ? calc.num : calc.result}</div>
+                <div id="display" className="text-end fs-3 mx-2 mt-2 px-1">{calc.num ? calc.num : calc.result}</div>
                 <div className="button-box m-1">
                     {btns.map((item) =>
-                        <div className="btn-primary text-center fs-2 border-0 m-1"
+                        <div className="rounded btn-primary text-center fs-2 border-0 m-1"
                             id={item[1]}
                             key={item[1]}
                             onClick={(item[1] === "negative") ? negativeClickHandler :
                                 (item[1] === "decimal") ? decimalClickHandler :
                                     (item[1] === "clear") ? clearClickHandler :
+                                    (item[1] === "clear-entry") ? clearEntryClickHandler :
                                         (item[1] === "add" || item[1] === "subtract" || item[1] === "multiply" || item[1] === "divide") ? operandClickHandler :
                                             (item[1] === "equals") ? () => equalsClickHandler() : numberClickHandler}>
                             {/* anonymous arrow function needed on equals Handler because it has a default parameter */}
